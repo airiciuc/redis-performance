65c13f75c2c7dc86b3366ebf091b277a9e65a8e2
Adrian Iriciuc <adrian.iriciuc@aurea.com>
2021-05-19 12:33:38 +0300

    push images to devfactory

--- build_image.sh
+++ build_image.sh
@@ -3,9 +3,9 @@
 ./gradlew installDist
 
 docker build -t pubsub_sub_cluster -f  Dockerfile_sub .
-docker tag pubsub_sub_cluster airiciuc/pubsub_sub_cluster
-docker push airiciuc/pubsub_sub_cluster
+docker tag pubsub_sub_cluster registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_sub_cluster
+docker push registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_sub_cluster
 
 docker build -t pubsub_pub_cluster -f  Dockerfile_pub .
-docker tag pubsub_pub_cluster airiciuc/pubsub_pub_cluster
-docker push airiciuc/pubsub_pub_cluster
+docker tag pubsub_pub_cluster registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_pub_cluster
+docker push registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_pub_cluster
