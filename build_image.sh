0b5107d883eef5dc6b613831adfef34842280f75
Adrian Iriciuc <adrian.iriciuc@aurea.com>
2021-05-20 09:17:52 +0300

    v2 config

--- build_image.sh
+++ build_image.sh
@@ -2,10 +2,10 @@
 
 ./gradlew installDist
 
-docker build -t pubsub_sub -f  Dockerfile_sub .
-docker tag pubsub_sub registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_sub
-docker push registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_sub
+docker build -t pubsub_sub_2 -f  Dockerfile_sub .
+docker tag pubsub_sub_2 registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_sub_2
+docker push registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_sub_2
 
-docker build -t pubsub_pub -f  Dockerfile_pub .
-docker tag pubsub_pub registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_pub
-docker push registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_pub
+docker build -t pubsub_pub_2 -f  Dockerfile_pub .
+docker tag pubsub_pub_2 registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_pub_2
+docker push registry2.swarm.devfactory.com/aurea/jive/redis-test/pubsub_pub_2
